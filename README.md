79781da13fc055fe4ff3dbbb8ae661b917c6f330
Ludger Paehler <ludger.paehler@tum.de>
2022-07-20 17:08:17 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Conda Recipe Repository
+
+## About this Repository
+
+Repository to host own Conda recipes, mostly centered around differentiable programming.
+
+## Content
+
+- Libenzyme, building Enzyme with Conda against LLVM 12
+
---
+++ recipes/libenzyme/build.sh
@@ -0,0 +1,18 @@
+export LLVM_DIR=$PREFIX/lib/cmake/llvm
+export LLVM_EXTERNAL_LIT=$PREFIX/bin/lit
+
+cd enzyme
+mkdir build
+cd build
+
+cmake -GNinja .. -DLLVM_DIR=$LLVM_DIR -DLLVM_EXTERNAL_LIT=$LLVM_EXTERNAL_LIT -DCMAKE_INSTALL_PREFIX=$PREFIX
+ninja
+
+# For tests to pass
+#cp $PREFIX/libexec/llvm/FileCheck $PREFIX/bin/FileCheck
+
+#ninja check-typeanalysis
+#ninja check-activityanalysis
+#ninja check-enzyme
+
+ninja install
